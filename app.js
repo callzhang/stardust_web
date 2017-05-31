049a37e806c750cc17c24dfebd40c8dd57357d8a
Lei Zhang <leizhang0121@gmail.com>
2017-06-01 00:53:01 +0800

    Updated the port number
    For AWS Beanstalk

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//express_demo.js 文件
+var port = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -7,7 +7,7 @@ app.set('views','./views');
 app.set("view engine","ejs");
 app.use('/public',express.static(__dirname + '/public'));
 app.use(bodyParser());
-var server = app.listen(8000, function () {
+var server = app.listen(port, function () {
     var port = server.address().port
      console.log('端口号:', port)
 })
